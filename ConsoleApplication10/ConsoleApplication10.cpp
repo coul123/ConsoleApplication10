d99d3d3501195e876315d5ed4701fad70bb2b33d
SimonT <piklol.you@gmail.com>
2024-10-25 11:40:14 +0500

    Рекурсивная функция фибоначи

--- ConsoleApplication10/ConsoleApplication10.cpp
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -1,6 +1,28 @@
 ﻿#include <iostream>
+using namespace std;
 
-int main()
+int fibonacci(int n) 
 {
-    std::cout << "Hello World!\n";
+    if (n == 0) {
+        return 0;
+    }
+    else if (n == 1) 
+    {
+        return 1;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+
+    cout << "Введите номер числа Фибоначчи: ";
+    cin >> n;
+
+    int result = fibonacci(n);
+    cout << "Число Фибоначчи с индексом " << n << " равно " << result << endl;
+
 }
