ed3b6a049eced0f6bf4b73378eba07b3ebda34de
SimonT <piklol.you@gmail.com>
2024-10-25 11:47:00 +0500

    Рекурсивная функция Аккермана

--- ConsoleApplication10/ConsoleApplication10.cpp
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -1,6 +1,22 @@
 ﻿#include <iostream>
+using namespace std;
 
-int main()
+int ackermann(int m, int n) 
 {
-    std::cout << "Hello World!\n";
+    if (m == 0) return n + 1;
+    if (m == 1) return n + 2;
+    if (m == 2) return 2 * n + 3;
+    if (m == 3) return 2 * n + 4; 
+
+    return ackermann(m - 1, ackermann(m, n - 1));
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    int m, n;
+    cout << "Введите два неотрицательных числа m и n: ";
+    cin >> m >> n;
+    cout << "A(" << m << ", " << n << ") = " << ackermann(m, n) << endl;
+    return 0;
 }
