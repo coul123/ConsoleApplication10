a60d2df709fc14749bccfd4e337168a63a281938
SimonT <piklol.you@gmail.com>
2024-10-25 11:58:13 +0500

    Добавил коменты

--- ConsoleApplication10/ConsoleApplication10.cpp
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -5,22 +5,25 @@ using namespace std;
 
 pair<int, int> find(const vector<vector<int>>& matrix, int target, int row, int col) 
 {
+    // Проверяем, выходим ли за границы массива
     if (row >= matrix.size()) 
     {
         return { -1, -1 }; 
     }
 
+    // Если достигли конца колонки, переходим к следующей строке
     if (col >= matrix[row].size()) 
     {
         return find(matrix, target, row + 1, 0);
     }
 
+    // Проверяем текущий элемент
     if (matrix[row][col] == target) 
     {
         return { row, col };
     }
 
-    return find(matrix, target, row, col + 1);
+    return find(matrix, target, row, col + 1); // Переход к следующему элементу в строке
 }
 
 int main() 
