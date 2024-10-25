ae5577a2171bb245f0c11d9b56ce74968ec93e74
SimonT <piklol.you@gmail.com>
2024-10-25 11:55:53 +0500

    рекурсивная функция поиска числа в двумерном массиве, возвращающую его индекс

--- ConsoleApplication10/ConsoleApplication10.cpp
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -1,6 +1,48 @@
 ﻿#include <iostream>
+#include <vector>
 
-int main()
+using namespace std;
+
+pair<int, int> find(const vector<vector<int>>& matrix, int target, int row, int col) 
 {
-    std::cout << "Hello World!\n";
+    if (row >= matrix.size()) 
+    {
+        return { -1, -1 }; 
+    }
+
+    if (col >= matrix[row].size()) 
+    {
+        return find(matrix, target, row + 1, 0);
+    }
+
+    if (matrix[row][col] == target) 
+    {
+        return { row, col };
+    }
+
+    return find(matrix, target, row, col + 1);
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    vector<vector<int>> matrix = 
+    {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int target;
+    cout << "Введите число для поиска: ";
+    cin >> target;
+
+    pair<int, int> result = find(matrix, target, 0, 0);
+
+    if (result.first != -1) {
+        cout << "Число " << target << " найдено по индексу: (" << result.first << ", " << result.second << ")" << '\n';
+    }
+    else {
+        cout << "Число " << target << " не найдено в массиве." << '\n';
+    }
 }
