f77e38903131cdbb0d6e6f06dcaef8416e5c8715
SimonT <piklol.you@gmail.com>
2024-10-25 12:01:00 +0500

    Добавил коменты

--- ConsoleApplication10/ConsoleApplication10.cpp
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int ackermann(int m, int n) 
 {
+    // Базовые случаи для функции Аккермана
     if (m == 0) return n + 1;
     if (m == 1) return n + 2;
     if (m == 2) return 2 * n + 3;
     if (m == 3) return 2 * n + 4; 
 
+    // Рекурсивный вызов для остальных случаев
     return ackermann(m - 1, ackermann(m, n - 1));
 }
 
